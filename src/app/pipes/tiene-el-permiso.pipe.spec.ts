7d379f1e4a5f339dd5b6d610640eac12137e0f64
Rafael Ramirez <rafa.yael@gmail.com>
2021-04-20 13:55:52 -0500

    preparado para modo producción

---
+++ src/app/pipes/tiene-el-permiso.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TieneElPermisoPipe } from './tiene-el-permiso.pipe';
+
+describe('TieneElPermisoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TieneElPermisoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
